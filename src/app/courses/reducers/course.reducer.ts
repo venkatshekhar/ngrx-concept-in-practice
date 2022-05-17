e6cc7f72c45bd99a872ca451f75fcefec56e78a0
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-17 13:42:23 +0530

    sorting course based on seq number using compareCourses

--- src/app/courses/reducers/course.reducer.ts
+++ src/app/courses/reducers/course.reducer.ts
@@ -1,14 +1,17 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
-import { Course } from "../model/course";
+import { compareCourses, Course } from "../model/course";
 
 export interface CourseState extends EntityState<Course>{
 
 }
 
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+    sortComparer: compareCourses,
+    // selectId: course => course.courseId  // if instead of id we have courseId since id is default we need to change using selectId
+});
 
 
 export const initialCoursesState = adapter.getInitialState();
