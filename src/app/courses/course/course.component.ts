95935f8f24d5b62de9ffd48842c30ba5a8f4a906
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-19 01:16:45 +0530

    adding on push change detection

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,10 +1,9 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
 import { Observable, of } from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom } from 'rxjs/operators';
-import { CoursesHttpService } from '../services/courses-http.service';
 import { CourseEntityService } from '../services/course-entity.service';
 import { LessonEntityService } from '../services/lesson-entity.service';
 
@@ -12,7 +11,8 @@ import { LessonEntityService } from '../services/lesson-entity.service';
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
