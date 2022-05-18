f5f1569534604e7ca45521153332318c35b2574e
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-18 23:46:50 +0530

    loading course only once using server loaded obs

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -1,20 +1,27 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { filter, first, map, tap } from "rxjs/operators";
 import { CourseEntityService } from "./course-entity.service";
 
 
 @Injectable()
 export class CoursesResolver implements Resolve<boolean>{
 
-    constructor(private courseService: CourseEntityService){
+    constructor(private courseService: CourseEntityService) {
 
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.courseService.getAll().pipe(
-            map(courses => !!courses)
+        
+        return this.courseService.loaded$.pipe(
+            tap(loaded => {
+                if(!loaded){
+                    this.courseService.getAll();
+                }
+            }),
+            filter(loaded => !!loaded),
+            first()
         );
     }
 
