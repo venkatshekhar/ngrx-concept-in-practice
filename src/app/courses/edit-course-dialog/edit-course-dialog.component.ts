82c278e105baf53fef4762a8e1cb1748ce9dd3e7
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-19 00:22:47 +0530

    create course in pessimistic way save in db and then show in UI

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -69,6 +69,16 @@ export class EditCourseDialogComponent {
       this.dialogRef.close();
 
     }
+    else if(this.mode == 'create'){
+      this.coursesService.add(course).subscribe(
+        newCourse =>{
+          console.log("New Course ", newCourse);
+
+          this.dialogRef.close();
+        }
+      );
+    }
+
 
   }
 
