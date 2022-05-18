769f4aaae650ba8e11dc54b9e44da139d7765924
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-19 01:07:26 +0530

    loading lesson with pagination

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,10 +1,10 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {Observable, of} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom } from 'rxjs/operators';
+import { CoursesHttpService } from '../services/courses-http.service';
 import { CourseEntityService } from '../services/course-entity.service';
 import { LessonEntityService } from '../services/lesson-entity.service';
 
@@ -17,6 +17,8 @@ import { LessonEntityService } from '../services/lesson-entity.service';
 export class CourseComponent implements OnInit {
 
   course$: Observable<Course>;
+
+  loading$: Observable<boolean>;
 
   lessons$: Observable<Lesson[]>;
 
@@ -39,13 +41,34 @@ export class CourseComponent implements OnInit {
       map(courses => courses.find(course => course.url == courseUrl))
     );
 
-    this.lessons$ = of([]);
+    this.lessons$ = this.lessonService.entities$
+      .pipe(
+        withLatestFrom(this.course$),
+        tap(([lessons, course]) => {
+          if (this.nextPage == 0) {
+            this.loadLessonsPage(course)
+          }
+        } 
+
+        ),
+        map(([lessons, course]) => lessons.filter(lesson => lesson.courseId == course.id))
+      );
+
+    this.loading$ = this.lessonService.loading$.pipe(delay(0));
+
+
 
   }
 
 
   loadLessonsPage(course: Course) {
+    this.lessonService.getWithQuery({
+      'courseId': course.id.toString(),
+      'pageNumber': this.nextPage.toString(),
+      'pageSize': '3'
+    });
 
+    this.nextPage += 1
   }
 
 }
